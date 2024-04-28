cb6ef11838b1c720c4742513dbdb073c63e8cf97
Phantom-Ant <darioantman19@gmail>
2024-04-28 21:15:27 +0200

    validazione, collisione iniziale, sprite

--- Pezzo.java
+++ Pezzo.java
@@ -17,16 +17,4 @@ class Pezzo{//TODO: rinomina attributi
         this.y = y;
         this.tipo = tipo;
     }
-    //ruotiamo il pezzo in senso orario
-    public void ruotaPezzo(){
-        int size = pezzo.length;
-        boolean[][] roteaPezzo = new boolean[size][size];
-        //
-        for(int i = 0; i<size; i++){
-            for(int j = 0;j<size;j++){
-                //calcolo dalla posizione della cella ruotata
-                roteaPezzo[j][size-1-i] = pezzo[i][j];
-            }
-        }
-    }
 }
