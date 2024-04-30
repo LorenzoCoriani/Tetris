eedad7d2afc6db38389326dbfcd790f1f14c67e3
Phantom-Ant <darioantman19@gmail>
2024-04-30 21:48:19 +0200

    collisioni fixate, cancella riga

--- Pezzo.java
+++ Pezzo.java
@@ -1,53 +1,61 @@
 import java.awt.*;
+import javax.swing.*;
+
 class Pezzo{//TODO: rinomina attributi
 
-    //schifo:
     static boolean pezzoI[][] = {{false,false, false, false},
-                         {true,  true,  true, true},
-                         {false, false, false, false},
-                         {false, false, false, false}};
+                                {true,  true,  true, true},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoI = new Color(0, 230, 254);
 
     static boolean pezzoJ[][] = {{true, false, false, false},
-                            {true,  true,  true, false},
-                            {false, false, false, false},
-                            {false, false, false, false}};
+                                {true,  true,  true, false},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoJ = new Color(24, 1, 255);
 
     static boolean pezzoL[][] = {{false, false, true, false},
-                            {true,  true,  true, false},
-                            {false, false, false, false},
-                            {false, false, false, false}};
+                                {true,  true,  true, false},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoL = new Color(255, 115, 8);
 
     static boolean pezzoO[][] = {{true, true, false, false},
-                            {true,  true,  false, false},
-                            {false, false, false, false},
-                            {false, false, false, false}};
+                                {true,  true,  false, false},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoO = new Color(255, 222, 0);
 
     static boolean pezzoS[][] = {{false, true, true, false},
-                            {true,  true,  false, false},
-                            {false, false, false, false},
-                            {false, false, false, false}};
+                                {true,  true,  false, false},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoS = new Color(102, 253, 0);
 
     static boolean pezzoZ[][] = {{true, true, false, false},
-                            {false,  true,  true, false},
-                            {false, false, false, false},
-                            {false, false, false, false}};
+                                {false,  true,  true, false},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoZ = new Color(254, 16, 60);
 
     static boolean pezzoT[][] = {{false, true, false, false},
-                            {true,  true,  true, false},
-                            {false, false, false, false},
-                            {false, false, false, false}};
+                                {true,  true,  true, false},
+                                {false, false, false, false},
+                                {false, false, false, false}};
     static Color tipoT = new Color(184, 2, 253);
 
 
     Color tipo;
     boolean pezzo[][];
     int x,y;
+
+    /*Pezzo(){
+        this.pezzo = ;
+        this.x = 4;
+        this.y = 0;
+        this.tipo = tipo;
+    }*/
 
     Pezzo(boolean pezzo[][], Color tipo){
         this.pezzo = pezzo;
@@ -63,7 +71,9 @@ class Pezzo{//TODO: rinomina attributi
         this.tipo = tipo;
     }
 
-    static Pezzo random(){
+    //metodi
+
+    static Pezzo random(){ //TODO: 7 bag randomizer
         boolean boolPezzo[][]= {{false, false, false, false},
                                 {false, false, false, false},
                                 {false, false, false, false},
@@ -107,6 +117,16 @@ class Pezzo{//TODO: rinomina attributi
         return pezzo;
     }
 
+    void disegna(JLabel lblDisplay[][], ImageIcon scaledIcon){
+		for(int i=0; i<4; i++){
+			for(int j=0; j<4; j++){
+				if(pezzo[i][j]){
+					lblDisplay[i+y][j+x].setBackground(tipo);
+					lblDisplay[i+y][j+x].setIcon(scaledIcon);
+				}
+			}
+		}
+	}
 }
 
 
--- Tetris.java
+++ Tetris.java
@@ -2,21 +2,20 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class Tetris{
+class Tetris{ //TODO: usare definizioni da https://tetris.wiki/Tetris_Guideline
 	final int width = 10;
 	final int height = 20;
-	final int frameWidth = 400;
-	final int frameHeight = 800;
+	final int frameRatio = 40;
 	int pausa=500; //millisecondi
 
 	JFrame frame;
 	JPanel pnlGioco;
 
-	boolean boolPezzo[][] = {{true, false,  false, false},
-							 {true, true,  false, false},
-							 {true, false, false, false},
+	boolean boolPezzo[][] = {{false, false, false, true},
+							 {false, false, true, true},
+							 {false, false, false, true},
 							 {false, false, false, false}};
-	Pezzo pezzo = Pezzo.random();//new Pezzo(boolPezzo, new Color(142, 68, 173));
+	Pezzo pezzo = Pezzo.random();
 
 	JLabel[][] lblDisplay;
 	Casella[][] blocchiSolidi;
@@ -25,7 +24,7 @@ class Tetris{
 
 	ImageIcon icon = new ImageIcon("./tile.png");
 
-	Image image = icon.getImage().getScaledInstance(frameHeight/height, frameWidth/width, Image.SCALE_SMOOTH); //TODO: fine tune this shit
+	Image image = icon.getImage().getScaledInstance(frameRatio, frameRatio, Image.SCALE_SMOOTH); //TODO: fine tune this shit
 	ImageIcon scaledIcon = new ImageIcon(image);
 
 	Timer timer;
@@ -52,17 +51,6 @@ class Tetris{
 				pnlGioco.add(lblDisplay[i][j]);
 			}
 		}
-		blocchiSolidi[19][0].colore = Color.RED;
-		blocchiSolidi[19][0].occupato = true;
-
-		blocchiSolidi[7][9].colore = Color.GREEN;
-		blocchiSolidi[7][9].occupato = true;
-
-		blocchiSolidi[12][3].colore = Color.BLUE;
-		blocchiSolidi[12][3].occupato = true;
-
-		//blocchiSolidi[10][6].colore = Color.YELLOW;
-		//blocchiSolidi[10][6].occupato = true;
 
 		//add
 		frame.add(pnlGioco,"Center");
@@ -74,7 +62,7 @@ class Tetris{
 		timer.start();
 
 		//impostazioni frame
-		frame.setSize(frameWidth, frameHeight);
+		frame.setSize(frameRatio*width, frameRatio*height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
@@ -122,7 +110,7 @@ class Tetris{
 					ruotaPezzo();
 				break;
 				default:
-					disegnaPezzo();
+					pezzo.disegna(lblDisplay, scaledIcon);
 			}
 		}
 		public void keyReleased(KeyEvent e){}
@@ -155,7 +143,7 @@ class Tetris{
             }
         }
 
-        disegnaPezzo();
+        pezzo.disegna(lblDisplay, scaledIcon);
     }
 
 	void spostaPezzo(int spostaX, int spostaY){
@@ -166,21 +154,15 @@ class Tetris{
 			pezzo.y += spostaY;
 
 
-		disegnaPezzo();
+		pezzo.disegna(lblDisplay, scaledIcon);
 
     }
-	void disegnaPezzo(){
-		for(int i=0; i<4; i++){
-			for(int j=0; j<4; j++){
-				if(pezzo.pezzo[i][j]){
-					lblDisplay[i+pezzo.y][j+pezzo.x].setBackground(pezzo.tipo);
-					lblDisplay[i+pezzo.y][j+pezzo.x].setIcon(scaledIcon);
-				}
-			}
-		}
-	}
 
-	void solidificaPezzo(){
+
+	void solidificaPezzo(){ //TODO: sistema robe che fanno cagare tipo boolean perso
+		boolean perso=false;
+		boolean riga=true;
+
 		for(int i=0; i<4; i++){
 			for(int j=0; j<4; j++){
 				if(pezzo.pezzo[i][j]){
@@ -188,19 +170,51 @@ class Tetris{
 					blocchiSolidi[pezzo.y+i][pezzo.x+j].colore = pezzo.tipo;
 					lblDisplay[pezzo.y+i][pezzo.x+j].setBackground(pezzo.tipo);
 					lblDisplay[i+pezzo.y][j+pezzo.x].setIcon(scaledIcon);//TODO: potresti sostituire scaledIcon mettere "animazione"
+					if(pezzo.y+i == 0)
+						perso = true;
 				}
 			}
 		}
-		pezzo = Pezzo.random(); //TODO: genera nuovo pezzo casuale
+
+		for(int i=0; i<height; i++){
+			riga=true;
+			for(int j=0; j<width&&riga; j++){
+				if(!blocchiSolidi[i][j].occupato)
+					riga = false;
+			}
+			if(riga)
+				cancellaRiga(i);
+		}
+		if(perso){
+			System.out.println("Hai perso");//TODO: aggiungi dialog
+			//pezzo = ; //TODO:distruggi il pezzo
+		}else{
+			pezzo = Pezzo.random(); //TODO: crealo solo dopo che le righe vanno in basso
+		}
 	}
 
-	boolean validaX(int spostaX){ //TODO: fixa per pezzo ruotato
+	void cancellaRiga(int riga){
+		for(int i=0; i<width; i++){
+			lblDisplay[riga][i].setBackground(Color.BLACK);
+			lblDisplay[riga][i].setIcon(null);
+		}
+
+		for(int i=riga-1; i>0; i--){
+			for(int j=0; j<width; j++){
+				blocchiSolidi[i+1][j].colore = blocchiSolidi[i][j].colore;
+				blocchiSolidi[i+1][j].occupato = blocchiSolidi[i][j].occupato;
+			}
+		}
+	}
+
+
+	boolean validaX(int spostaX){
 		boolean valido=true;
 
-		//collisioni bordi //programmato a cazzo
+		//collisioni bordi
 		for(int i=0; i<4 &&valido; i++){
 			for(int j=0; j<4 &&valido; j++){
-				if(pezzo.pezzo[i][j] && !(pezzo.x+spostaX>=0 && pezzo.x+spostaX+j<width)){
+				if(pezzo.pezzo[i][j] && !(pezzo.x+spostaX+j>=0 && pezzo.x+spostaX+j<width)){
 					valido = false;
 				}
 			}
@@ -221,7 +235,7 @@ class Tetris{
 	boolean validaY(int spostaY){
 		boolean valido=true;
 
-		//collisioni bordi //programmato a cazzo
+		//collisioni bordi
 		for(int i=0; i<4 &&valido; i++){
 			for(int j=0; j<4 &&valido; j++){
 				if(pezzo.pezzo[i][j] && !(pezzo.y+spostaY+i<height)){
